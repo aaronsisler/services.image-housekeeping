68040aafb139a30fc90f786c91359e4ac497b435
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-04-22 20:29:53 -0400

    Adding in change for handler to at least pull ECR images

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 1. Create the lambda
 2. Create the Cloudwatch rule for listening to ECR and calling the lambda
+
+```bash
+aws ecr describe-images --repository-name applications.paper-trail.financial-data-ingestion-service
+```
--- app/src/handler/event_handler.py
+++ app/src/handler/event_handler.py
@@ -1,4 +1,25 @@
+import json
+
+import boto3
+
+
 def handle_new_image(event, _context):
     print(event)
 
-    return
+    ecr_client = boto3.client('ecr')
+
+    image_details = event['detail']
+    repository_name = image_details['repository-name']
+
+    response = ecr_client.describe_images(
+        repositoryName=repository_name
+    )
+
+    print(response)
+
+    # Need to loop over the images and delete the ones that don't have imageTag as latest
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'Images that are not the latest have been deleted from {repository_name}')
+    }
